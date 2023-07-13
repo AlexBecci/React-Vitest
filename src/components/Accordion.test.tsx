fe27da724013b2ff719be788fa5c90eb513dd52d
AlexBecci <beccialex2002@gmail.com>
2023-07-13 14:31:25 -0300

    testing

---
+++ src/components/Accordion.test.tsx
@@ -0,0 +1,7 @@
+import Accordion from "./Accordion.tsx";
+
+describe('Accordion', () => {
+    test("should add two numbers", () => {
+        expect(1 + 1).toBe(2);
+    })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types ="vitest" />
+/// <reference types ="Vite/client" />
+
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -8,5 +10,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment:"jsdom",
+    globals: true,
   }
 });
